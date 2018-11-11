6bfac89127db4fe09db97d033e6e693a9ac0c4c2
Cian Feldshtein <cian.feldshtein@mycit.ie>
2018-11-11 22:25:02 +0000

    Second commit to sensor file. Setting up the connections broadcast and unicast

--- sensor_second_commit.c
+++ sensor_second_commit.c
@@ -1,7 +1,7 @@
 /**
  * \file
  *         Sensor for Assesment
- *         Commit 1: Setup temperature reading
+ *         Commit 2: Setup the connections broadcast and unicast
  * \author
  *         Cian Feldshtein
  *	       cian.feldshtein@mycit.ie
@@ -24,6 +24,19 @@ AUTOSTART_PROCESSES(&sensor_information_process);
 static int temp;
 static int count;
 
+static struct unicast_conn unicast;
+static const struct unicast_callbacks unicast_callbacks = {};
+
+static struct broadcast_conn broadcast;
+
+
+static void
+broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+{
+	
+}
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+
 //Define the process code
 PROCESS_THREAD(sensor_information_process, ev, data)
 {
@@ -34,7 +47,15 @@ PROCESS_THREAD(sensor_information_process, ev, data)
 	//Variable to store temperature reading
 	static int valTemp;
 
+	//closes connections when process exits
+	PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+	PROCESS_EXITHANDLER(unicast_close(&unicast);)
+
 	PROCESS_BEGIN();
+	
+	//Open connections
+	broadcast_open(&broadcast, 123, &broadcast_call);
+	unicast_open(&uc, 134, &unicast_callbacks);
 	
 
 	while(1){
@@ -53,7 +74,7 @@ PROCESS_THREAD(sensor_information_process, ev, data)
 		//Reading temperature
 		valTemp = ((sht11_sensor.value(SHT11_SENSOR_TEMP) / 10) - 396) / 10;	
 
-		//temp is a combination of current temp value and temperature read in. Added together and divided by a counter to get the average
+		//temp a combination of current temp value and temperature read in. Added together and divided by a counter to get average
 		temp = temp + valTemp;
 
 		//Count increments by 1 for every temperature read
